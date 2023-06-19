3232bcff59ced02595530219254158539e05e382
Rodrigo Lemes <rodrigo.lemes8@icloud.com>
2023-06-19 17:36:31 -0300

    Treinando funcao

--- funcao/index.js
+++ funcao/index.js
@@ -1,5 +1,37 @@
-//Função de Declaração
+function somar () {
+  const n1 = document.getElementById("n1").value
+  const n2 = document.getElementById("n2").value
 
-function soma() {
-  
+  const soma = parseFloat(n1) + parseFloat(n2)
+  document.getElementById("resultadoSoma").innerHTML = "O resultado é: " + soma
+
+}
+
+
+function subtrair () {
+  const n1 = document.getElementById("n1").value
+  const n2 = document.getElementById("n2").value
+
+  const subtração = parseFloat(n1) - parseFloat(n2)
+  document.getElementById("resultadoSoma").innerHTML = "O resultado é: " + subtração
+
+}
+
+function multiplicar () {
+  const n1 = document.getElementById("n1").value
+  const n2 = document.getElementById("n2").value
+
+
+  const muiltiplicação = parseFloat(n1) * parseFloat(n2)
+  document.getElementById("resultadoSoma").innerHTML = "O resultado é: " + muiltiplicação
+
+}
+
+function dividir () {
+  const n1 = document.getElementById("n1").value
+  const n2 = document.getElementById("n2").value
+
+  const divição = parseFloat(n1) / parseFloat(n2)
+  document.getElementById("resultadoSoma").innerHTML = "O resultado é: " + divição
+
 }
