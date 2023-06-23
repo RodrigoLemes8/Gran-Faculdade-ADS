94d940ff7e03ea362b923ad3b745e2544c5a58bc
Rodrigo Lemes <rodrigo.lemes8@icloud.com>
2023-06-23 15:40:08 -0300

    Criação do tabuleiro

--- Imersão JavaScript – Coleções e Funções/Jogo da Velha/index.js
+++ Imersão JavaScript – Coleções e Funções/Jogo da Velha/index.js
@@ -9,8 +9,18 @@ let coluna
 
 
 function iniciar() {
+  tabuleiro = []
+  bord = document.getElementById("borad")
+  aviso = document.getElementById("aviso")
+  jogador = 1
+  for (let i = 0; i < 3 ; i++) {
+    tabuleiro[1] = [];
+    for(let j = 0; j < 3; j++){
+      tabuleiro[i][j] = 0
 
+    }
 
+  }
 }
 function exibir() {
 
